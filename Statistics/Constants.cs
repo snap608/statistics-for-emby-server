ce379046f2fc7ec1e9b09007616b6c5f3a508925
Mikhael <mikhael@uping.be>
2017-02-26 10:50:26 +0100

    Fixed #5, rename Show to TV Series

--- Statistics/Constants.cs
+++ Statistics/Constants.cs
@@ -10,25 +10,25 @@
         internal const string FavoriteMovieGenres = "Favorite Movie Genres";
         internal const string favoriteShowGenres = "Favorite Show Genres";
         internal const string LastSeenMovies = "Last Seen Movies";
-        internal const string LastSeenShows = "Last Seen Shows";
+        internal const string LastSeenShows = "Last Seen TV Series";
         internal const string TotalWatched = "Total Time Watched";
         internal const string TotalWatchableTime = "Total Watchable Time";
         internal const string FavoriteYears = "Favorite Movie Years";
         internal const string MostActiveUsers = "Most Active Users";
         internal const string TotalMovies = "Total Movies";
-        internal const string TotalShows = "Total Shows";
+        internal const string TotalShows = "Total TV Series";
         internal const string TotalEpisodes = "Total Episodes";
         internal const string MediaQualities = "Media Qualities";
-        internal const string LongestMovie = "Longest Movie";
-        internal const string LongestShow = "Longest Show";
+        internal const string LongestMovie = "Longest Movie Runtime";
+        internal const string LongestShow = "Longest TV Series Runtime";
         internal const string BiggestMovie = "Largest Movie";
-        internal const string BiggestShow = "Largest Show";
+        internal const string BiggestShow = "Largest TV Series";
 
 
         //Help text for stats
         internal const string HelpMostActiveUsers = "Top 5 users that are the most active on the Emby server. This includes viewing movies and episodes.";
         internal const string HelpUserTotalMovies = "Total movies this user can see in his/her Emby library.";
-        internal const string HelpUserTotalShows = "Total shows this user can see in his/her Emby library.";
+        internal const string HelpUserTotalShows = "Total TV Series this user can see in his/her Emby library.";
         internal const string HelpUserTotalEpisode = "Total episodes this user can see in his/her Emby library.";
         internal const string HelpUserToMovieYears = "Top 5 years the user watched movies.";
         internal const string HelpUserTopMovieGenres = "Top 5 movie genres the user watched.";
